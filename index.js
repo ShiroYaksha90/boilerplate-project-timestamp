970d9e7569186235a83defefe2901f48025f07f8
ShiroYaksha90 <aljazaeribasim@gmail.com>
2026-01-31 07:28:40 +0300

    fix: format CORS options and update API response to include unix and utc timestamps

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -17,11 +17,25 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
+const options = {
+  weekday: 'short', // "Mon"
+  year: 'numeric', // "2026"
+  month: 'long',   // "January"
+  day: 'numeric',   // "26"
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  timeZoneName: 'shortOffset' // e.g., GMT-5
+};
+const date = new Date()
+const miliSeconds = date.getTime()
+const unixTime = Math.floor(miliSeconds / 1000)
+const formattedDateLocal = new Intl.DateTimeFormat(undefined, options).format(date);
+console.log(formattedDateLocal);
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ "unix": unixTime, "utc": formattedDateLocal });
 });
 
 
